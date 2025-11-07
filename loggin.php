a4677d7afceefd31d2c46808404629ee01026882
virrew <victor.wahlstromm@gmail.com>
2025-11-07 18:05:00 +0100

    Ändringar

---
+++ loggin.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="POST">
+        <label for="username">Username:</label>
+        <input type="text" id="username" name="username" required>
+        <br>
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password" required>
+        <br>
+        <input type="submit" value="Login">
+    </form>
+    <form action="register.php" method="POST">
+        <label for="create">Create Account</label>
+        <input type="submit" value="Register">
+    </form>
+</body>
+</html>
