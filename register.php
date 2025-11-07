2f819ccb27a69fb45aaca5281e94838869eac926
virrew <victor.wahlstromm@gmail.com>
2025-11-07 18:35:13 +0100

    Update register.php

--- register.php
+++ register.php
@@ -9,7 +9,30 @@ session_start();
     <title>Document</title>
 </head>
 <body>
-    <form action="">
+    <form action="index.php" method="POST">
+        <label for="username">Username:</label>
+        <input type="text" id="username" name="username" required>
+        <br>
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password" required>
+        <br>
     </form>
+    <form action="register.php" method="POST">
+        <label for="create">Create Account</label>
+        <input type="submit" value="Register">
+    </form>
+<?php
+    $pdo = new PDO('mysql:dbname=grupp6;host=localhost', 'wwwit-utv', 'Pangolin!24');
+    $pdo->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING );
+    
+    if(isset($_POST['username'])){
+        $querystring = "INSERT INTO logindetails (username, password) VALUES (:username, :password)";
+        $stmt = $pdo->prepare($querystring);
+        $stmt->bindParam(':username', $_POST['username']);
+        $stmt->bindParam(':password', ($_POST['password']));
+        $stmt->execute();
+        echo "User registered successfully.";
+    }
+?>
 </body>
 </html>
