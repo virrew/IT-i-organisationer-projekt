2ff5745d8092521c149c5f00a14b7f6d25e474ec
Amanda-1111 <laisdaone@gmail.com>
2025-11-20 18:25:13 +0100

    Update Kontakt.php

---
+++ Kontakt.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="sv">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<title>Kontaktsida</title>
+</head>
+<body>
+
+
+<h1><h1>
+
+<form method="post" action="kontakt.php">
+
+  <fieldset>
+  <input type="submit" value="Skicka in">
+
+</body>
+</html>
