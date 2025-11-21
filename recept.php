5b6d9d4f1a4af26c95b118149e3fa380f228e9ee
virrew <victor.wahlstromm@gmail.com>
2025-11-21 15:03:50 +0100

    Hämtat mina patieter och sjukskötersker

--- recept.php
+++ recept.php
@@ -1,6 +1,132 @@
-<?php 
-session_start(); 
-?> 
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+$cookiepath = "/tmp/cookies.txt";
+$tmeout = 3600; // (3600=1hr)
+// här sätter ni er domän
+$baseurl = 'http://193.93.250.83:8080/'; 
+
+try {
+  $ch = curl_init($baseurl . 'api/method/login');
+} catch (Exception $e) {
+  echo 'Caught exception: ',  $e->getMessage(), "\n";
+}
+
+curl_setopt($ch, CURLOPT_POST, true);
+//  ----------  Här sätter ni era login-data ------------------ //
+curl_setopt($ch, CURLOPT_POSTFIELDS, '{"usr":"a24vicwa@student.his.se", "pwd":"Rolleman1!"}'); 
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json', 'Accept: application/json'));
+curl_setopt($ch, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
+curl_setopt($ch, CURLOPT_COOKIEJAR, $cookiepath);
+curl_setopt($ch, CURLOPT_COOKIEFILE, $cookiepath);
+curl_setopt($ch, CURLOPT_TIMEOUT, $tmeout);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+//curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+$response = curl_exec($ch);
+$response = json_decode($response, true);
+
+$error_no = curl_errno($ch);
+$error = curl_error($ch);
+curl_close($ch);
+
+if (!empty($error_no)) {
+  echo "<div style='background-color:red'>";
+  echo '$error_no<br>';
+  var_dump($error_no);
+  echo "<hr>";
+  echo '$error<br>';
+  var_dump($error);
+  echo "<hr>";
+  echo "</div>";
+}
+echo "<div style='background-color:lightgray; border:1px solid black'>";
+echo '$response<br><pre>';
+echo print_r($response) . "</pre><br>";
+echo "</div>";
+$ch = curl_init($baseurl . 'api/resource/Medication%20Request?fields=[%22practitioner_name%22]&filters=[[%22practitioner_name%22,%22LIKE%22,%22%G6%%22]]'); 
+
+// man kan även specificera vilka fält man vill se
+// urlencode krävs när du har specialtecken eller mellanslag  
+// $ch = curl_init($baseurl . 'api/resource/User?fields='. urlencode('["name", "first_name", "last_login"]'));
+// det funkerar lika bra att ta bort mellanslaget i denna fråga
+// $ch = curl_init($baseurl . 'api/resource/User?fields=["name","first_name","last_login"]');
+
+//jag kör en get request, ibland vill man kanske köra en annan typ av request, och ibland så beöver man ha med postfields
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json', 'Accept: application/json'));
+curl_setopt($ch, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
+curl_setopt($ch, CURLOPT_COOKIEJAR, $cookiepath);
+curl_setopt($ch, CURLOPT_COOKIEFILE, $cookiepath);
+curl_setopt($ch, CURLOPT_TIMEOUT, $tmeout);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+
+$response = curl_exec($ch);
+//echo $response;
+$response = json_decode($response, true);
+
+$error_no = curl_errno($ch);
+$error = curl_error($ch);
+curl_close($ch);
+
+if (!empty($error_no)) {
+  echo "<div style='background-color:red'>";
+  echo '$error_no<br>';
+  var_dump($error_no);
+  echo "<hr>";
+  echo '$error<br>';
+  var_dump($error);
+  echo "<hr>";
+  echo "</div>";
+}
+echo "<div style='background-color:lightgray; border:1px solid black'>";
+echo '$response<br><pre>';
+echo print_r($response) . "</pre><br>";
+echo "</div>";
+
+//här väljer jag att loopa över alla poster i [data] och för varje resultat så skriver jag ut name
+echo "<strong>LISTA:</strong><br>";
+foreach($response['data'] AS $key => $value){
+  echo $value["practitioner_name"]."<br>";
+}
+
+ //Hämtar patienter som har utskrivna recept
+
+ $patient_url = $baseurl . 'api/resource/Medication%20Request?fields=[%22patient_name%22]&filters[[%22patient_name%22,%20%22LIKE%22,%20%22%G6%%22]]';
+
+$ch = curl_init($patient_url);
+
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json', 'Accept: application/json'));
+curl_setopt($ch, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
+curl_setopt($ch, CURLOPT_COOKIEJAR, $cookiepath);
+curl_setopt($ch, CURLOPT_COOKIEFILE, $cookiepath);
+curl_setopt($ch, CURLOPT_TIMEOUT, $tmeout);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+$response = curl_exec($ch);
+$response = json_decode($response, true);
+
+curl_close($ch);
+
+// Visa responsen för debugging
+echo "<div style='background-color:lightgray; border:1px solid black'>";
+echo '<pre>';
+print_r($response);
+echo '</pre>';
+echo "</div>";
+
+// Loopar igenom patienterna
+echo "<strong>Patientlista:</strong><br>";
+foreach ($response['data'] as $patient) {
+    echo $patient["patient_name"] . "<br>";
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
