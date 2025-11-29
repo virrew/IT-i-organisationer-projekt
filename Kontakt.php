2701c43c0a79552b2a82e2163c6df0a32938cb71
Amanda-1111 <laisdaone@gmail.com>
2025-11-29 14:08:51 +0100

    Update Kontakt.php

--- Kontakt.php
+++ Kontakt.php
@@ -72,7 +72,7 @@ echo print_r($response) . "</pre><br>";
 echo "</div>";
 
 
-$ch = curl_init($baseurl . 'api/resource/Healthcare%20Practitioner?fields=[%22first_name%22,%20%22name%22]&filters=[[%22first_name%22,%22LIKE%22,%22%G6%%22]]'); länk grejer ändra
+$ch = curl_init($baseurl . 'api/resource/'); länk grejer ändra
 
 // man kan även specificera vilka fält man vill se
 // urlencode krävs när du har specialtecken eller mellanslag  
