dbd6b90c470d7c7e9de04f55560283fdc70d349b
Amanda-1111 <laisdaone@gmail.com>
2025-12-01 19:56:24 +0100

    Update Kontakt.php

--- Kontakt.php
+++ Kontakt.php
@@ -89,29 +89,11 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
 $response = curl_exec($ch);
 $response = json_decode($response, true);
-curl_close($ch);
-
-
-//jag kör en get request, ibland vill man kanske köra en annan typ av request, och ibland så beöver man ha med postfields
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
-
-curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json', 'Accept: application/json'));
-curl_setopt($ch, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
-curl_setopt($ch, CURLOPT_COOKIEJAR, $cookiepath);
-curl_setopt($ch, CURLOPT_COOKIEFILE, $cookiepath);
-curl_setopt($ch, CURLOPT_TIMEOUT, $tmeout);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-
-
-$response = curl_exec($ch);
-//echo $response;
-$response = json_decode($response, true);
 
 $error_no = curl_errno($ch);
 $error = curl_error($ch);
 curl_close($ch);
+
 
 if (!empty($error_no)) {
   echo "<div style='background-color:red'>";
@@ -128,18 +110,13 @@ echo '$response<br><pre>';
 echo print_r($response) . "</pre><br>";
 echo "</div>";
 
-//här väljer jag att loopa över alla poster i [data] och för varje resultat så skriver jag ut name
-echo "<strong>LISTA:</strong><br>";
-if (isset($response['data']) && is_array($response['data'])) {
+//här väljer jag att loopa över alla poster i [data] och loopar sedan igen för alla rader inom det
     foreach ($response['data'] as $array) {
       foreach($array as $a => $row)
         echo $row . "<br>";
     }
-} else {
-    echo "Inga data kunde hämtas.";
-}
+ 
 
-echo "insert into tabell(name, owner, creation, modified, modif) values()"
 
 ?>
 <head>
@@ -153,86 +130,143 @@ echo "insert into tabell(name, owner, creation, modified, modif) values()"
 <h1>Formulär för bemötande<h1>
 
 
+
+<form method="post" action="kontakt.php">
+
+  <input type="number" id="age" name="age">
+            <label for="age">Hur gammal är du?</label><br>
+            
+            <input type="text" id="gender" name="gender">
+            <label for="gender">Kön...</label><br>
+
+
+   <fieldset>
+            <legend>Allmänt om ditt besök på vårdcentralen</legend>
+            
+            
+            <p>Fick du möjlighet att ställa frågorna du önskade?</p>
+            <input type="radio" id="Yes" name="able" value="1">
+            <label for="Yes">Ja</label><br>
+            
+            <input type="radio" id="No" name="able" value="0">
+            <label for="No">Nej</label><br>
+            
+
+
+            <p>Var det enkelt att ta till sig informationen under vårdmötet?</p>
+            <input type="radio" id="Yes" name="easy" value="1">
+            <label for="Yes">Ja</label><br>
+            
+            <input type="radio" id="No" name="easy" value="0">
+            <label for="No">Nej</label><br>
+            
+
+            
+            <p>Är du nöjd med det sätt du kan komma i kontakt med vårdcentralen?</p>
+            <input type="radio" id="Yes" name="happy" value="1">
+            <label for="Yes">Ja</label><br>
+            
+            <input type="radio" id="No" name="happy" value="0">
+            <label for="No">Nej</label><br>
+            
+
+            
+            <p>Fick du besöka vårdcentralen inom en rimlig tid?</p>
+            <input type="radio" id="Yes" name="meet" value="1">
+            <label for="Yes">Ja</label><br>
+            
+            <input type="radio" id="No" name="meet" value="0">
+            <label for="No">Nej</label><br>
+            
+
+            
+            <p>Var väntan i väntrummet längre än 20 min?</p>
+            <input type="radio" id="Yes" name="time" value="1">
+            <label for="Yes">Ja</label><br>
+            
+            <input type="radio" id="No" name="time" value="0">
+            <label for="No">Nej</label><br>
+        
+        </fieldset>
+
+         <fieldset>
+            <legend>Information och kunskap</legend>
+            
+            
+            <p>Fick du tillräckligt med information om din behandling och eventuella bieffekter?</p>
+            <input type="radio" id="Yes" name="info" value="1">
+            <label for="Yes">Ja</label><br>
+            
+            <input type="radio" id="No" name="info" value="0">
+            <label for="No">Nej</label><br>
+            
+
+            <p>Om du ställde frågor till vårdpersonalen fick du svar som du förstod?</p>
+            <input type="radio" id="Yes" name="understand" value="1">
+            <label for="Yes">Ja</label><br>
+            
+            <input type="radio" id="No" name="understand" value="0">
+            <label for="No">Nej</label><br>
+
+
+
+            <p>Förklarade läkaren/sjuksköterskan/annan vårdpersonal behandlingen på ett sätt som du förstod?</p>
+            <input type="radio" id="Yes" name="explain" value="1">
+            <label for="Yes">Ja</label><br>
+            
+            <input type="radio" id="No" name="explain" value="0">
+            <label for="No">Nej</label><br>
+            
+
+            
+            <p>Blev du informerade om ett kommande världsförlopp?</p>
+            <input type="radio" id="Yes" name="did" value="1">
+            <label for="Yes">Ja</label><br>
+            
+            <input type="radio" id="No" name="did" value="0">
+            <label for="No">Nej</label><br>
+        
+        </fieldset>
+        <label for="extra">Är det något från de ovannämnda frågorna som du specifikt vill utveckla? </label><br>
+   <textarea maxlength="500" id="extra"></textarea>
+
+  <input type="submit" value="Skicka in">
+</form>
+
 <?php
+$ch = curl_init(
+    $baseurl . "api/resource/G6FeedbackForm"
+);
 
-echo "<form method='post' action='kontakt.php'>";
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+curl_setopt($ch, CURLOPT_POSTFIELDS, '{
+"":"",
 
-echo "<input type='number' id='age' name='age'>";
-echo "<label for='age'>Hur gammal är du?</label><br>";
+}');
 
-echo "<input type='text' id='gender' name='gender'>";
-echo "<label for='gender'>Kön...</label><br>";
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json', 'Accept: application/json'));
+curl_setopt($ch, CURLOPT_COOKIEJAR, $cookiepath);
+curl_setopt($ch, CURLOPT_COOKIEFILE, $cookiepath);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-echo "<fieldset>";
-echo "<legend>Allmänt om ditt besök på vårdcentralen</legend>";
+$response = curl_exec($ch);
+$response = json_decode($response, true);
 
-echo "<p>Fick du möjlighet att ställa frågorna du önskade?</p>";
-echo "<input type='radio' id='Yes1' name='Quest' value='Yes'>";
-echo "<label for='Yes1'>Ja</label><br>";
-echo "<input type='radio' id='No1' name='Quest' value='No'>";
-echo "<label for='No1'>Nej</label><br>";
+$error_no = curl_errno($ch);
+$error = curl_error($ch);
+curl_close($ch);
 
-echo "<p>Var det enkelt att ta till sig informationen under vårdmötet?</p>";
-echo "<input type='radio' id='Yes2' name='info' value='Yes'>";
-echo "<label for='Yes2'>Ja</label><br>";
-echo "<input type='radio' id='No2' name='info' value='No'>";
-echo "<label for='No2'>Nej</label><br>";
 
-echo "<p>Är du nöjd med det sätt du kan komma i kontakt med vårdcentralen?</p>";
-echo "<input type='radio' id='Yes3' name='nojd' value='Yes'>";
-echo "<label for='Yes3'>Ja</label><br>";
-echo "<input type='radio' id='No3' name='nojd' value='No'>";
-echo "<label for='No3'>Nej</label><br>";
-
-echo "<p>Fick du besöka vårdcentralen inom en rimlig tid?</p>";
-echo "<input type='radio' id='Yes4' name='rimlig' value='Yes'>";
-echo "<label for='Yes4'>Ja</label><br>";
-echo "<input type='radio' id='No4' name='rimlig' value='No'>";
-echo "<label for='No4'>Nej</label><br>";
-
-echo "<p>Var väntan i väntrummet längre än 20 min?</p>";
-echo "<input type='radio' id='Yes5' name='rum' value='Yes'>";
-echo "<label for='Yes5'>Ja</label><br>";
-echo "<input type='radio' id='No5' name='rum' value='No'>";
-echo "<label for='No5'>Nej</label><br>";
-
-echo "</fieldset>";
-
-echo "<fieldset>";
-echo "<legend>Information och kunskap</legend>";
-
-echo "<p>Fick du tillräckligt med information om din behandling och eventuella bieffekter?</p>";
-echo "<input type='radio' id='Yes6' name='be' value='Yes'>";
-echo "<label for='Yes6'>Ja</label><br>";
-echo "<input type='radio' id='No6' name='be' value='No'>";
-echo "<label for='No6'>Nej</label><br>";
-
-echo "<p>Om du ställde frågor till vårdpersonalen fick du svar som du förstod?</p>";
-echo "<input type='radio' id='Yes7' name='stod' value='Yes'>";
-echo "<label for='Yes7'>Ja</label><br>";
-echo "<input type='radio' id='No7' name='stod' value='No'>";
-echo "<label for='No7'>Nej</label><br>";
-
-echo "<p>Förklarade läkaren/sjuksköterskan/annan vårdpersonal behandlingen på ett sätt som du förstod?</p>";
-echo "<input type='radio' id='Yes8' name='klar' value='Yes'>";
-echo "<label for='Yes8'>Ja</label><br>";
-echo "<input type='radio' id='No8' name='klar' value='No'>";
-echo "<label for='No8'>Nej</label><br>";
-
-echo "<p>Blev du informerad om ett kommande vårdförlopp?</p>";
-echo "<input type='radio' id='Yes9' name='kommande' value='Yes'>";
-echo "<label for='Yes9'>Ja</label><br>";
-echo "<input type='radio' id='No9' name='kommande' value='No'>";
-echo "<label for='No9'>Nej</label><br>";
-
-echo "</fieldset>";
-
-echo "<label for='extra'>Är det något från de ovannämnda frågorna som du specifikt vill utveckla?</label><br>";
-echo "<textarea maxlength='500' id='extra' name='extra'></textarea>";
-
-echo "<input type='submit' value='Skicka in'>";
-
-echo "</form>";
+if (!empty($error_no)) {
+  echo "<div style='background-color:red'>";
+  echo '$error_no<br>';
+  var_dump($error_no);
+  echo "<hr>";
+  echo '$error<br>';
+  var_dump($error);
+  echo "<hr>";
+  echo "</div>";
+}
 
 ?>
 
