063aa13dc456ef04e29305a99ba9229467c53997
Amanda-1111 <laisdaone@gmail.com>
2025-12-05 09:23:52 +0100

    Update kontaktformulär.php

--- kontaktformulär.php
+++ kontaktformulär.php
@@ -36,6 +36,17 @@ $error_no = curl_errno($ch);
 $error = curl_error($ch);
 curl_close($ch);
 
+if (!empty($error_no)) {
+  echo "<div style='background-color:red'>";
+  echo '$error_no<br>';
+  var_dump($error_no);
+  echo "<hr>";
+  echo '$error<br>';
+  var_dump($error);
+  echo "<hr>";
+  echo "</div>";
+}
+
 
 
 
