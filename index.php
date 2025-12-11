0b124431533bfff5f39ad1766b1908a77c98c9a0
dilyaraabdstudent-bit <dilyara.abd.student@gmail.com>
2025-12-11 10:50:07 +0100

    Update index.php

--- index.php
+++ index.php
@@ -307,7 +307,7 @@ $appointments = $data['data'] ?? [];   // Kommande bokningar
                 </div>
 
                 <div style="margin-top:12px;">
-                    <a href="Avboka min tid.php?id=<?= urlencode($a['name']) ?>" class="mini-btn mini-delete">❌ Avboka</a>
+                    <a href="Avboka.php?id=<?= urlencode($a['name']) ?>" class="mini-btn mini-delete">❌ Avboka min tid</a>
                     <a href="omboka.php?id=<?= urlencode($a['name']) ?>" class="mini-btn mini-edit">🔁 Omboka</a>
                 </div>
 
