0989d17039f91985d5b52953771b310efaf32219
Amanda-1111 <laisdaone@gmail.com>
2025-12-18 13:22:17 +0100

    tagit bort länk till kontakt

--- recept.php
+++ recept.php
@@ -434,7 +434,6 @@ if (!empty($error_no)) {
             <a href="journal.php">Min journal</a>
             <a href="recept.php">Mina recept</a>
             <a href="kontaktformulär.php">Boka tid här</a>
-            <a href="Kontakt.php">Kontakt</a>
 
             <!-- Höger sida – användarnamn + logga ut -->
             <span class="nav-user"><?= htmlspecialchars($_SESSION['username']) ?></span>
